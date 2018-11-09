5595a3f8dca6cb2d9000f7554e59edb45ae84682
ExtosNex <ExtosNex@users.noreply.github.com>
2018-11-09 13:43:34 +1100

    Update Counter.ino

--- Counter.ino
+++ Counter.ino
@@ -1,18 +1,65 @@
-const int buttonPin2 = 2;
-const int buttonPin3 = 3;
-const int A = 4;
-const int B = 5;
-const int C = 6;
-const int D = 7;
-const int E = 8;
-const int F = 9;
-const int O= 10;
-int count = 0;   
+   /*
+Seven Segment Display Layout
+
+       A
+     _____
+  F |     | B
+    |  O  |
+     ―――――
+    |     | C
+  E |     |
+     ‾‾‾‾‾
+       D
+
+       T
+     _____
+  Y |     | U
+    |  Z  |
+     ―――――
+    |     | V
+  X |     |
+     ‾‾‾‾‾
+       W
+
+*/
+
+// Define Button Pins
+const int buttonPin2 = 50;
+const int buttonPin3 = 52;
+
+// Define Pin Outputs for Seven Segment Display
+const int A = 30;
+const int B = 32;
+const int C = 34;
+const int D = 36;
+const int E = 38;
+const int F = 40;
+const int O = 42;
+
+const int T = 2;
+const int U = 3;
+const int V = 4;
+const int W = 5;
+const int X = 6;
+const int Y = 7;
+const int Z = 8;
+
+
+
+// Counter Var
+int count1 = 0;
+int count2 = 0;
+
+//Button State Var
 int buttonState2;
 int buttonState3;
 
+
+//Defines Variables
 void setup() {
-  Serial.begin(9600);       
+  //debugging
+  Serial.begin(9600);
+  
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
   pinMode(A, OUTPUT);
@@ -22,32 +69,79 @@ void setup() {
   pinMode(E, OUTPUT);
   pinMode(F, OUTPUT);
   pinMode(O, OUTPUT);
+  pinMode(T, OUTPUT);
+  pinMode(U, OUTPUT);
+  pinMode(V, OUTPUT);
+  pinMode(W, OUTPUT);
+  pinMode(X, OUTPUT);
+  pinMode(Y, OUTPUT);
+  pinMode(Z, OUTPUT);
 }
            
 
 void loop() {
+  //Checks Button State
   buttonState2 = digitalRead(buttonPin2);
   buttonState3 = digitalRead(buttonPin3);
 
+//If button2 is pressed, add +1 to count Var
   if (buttonState2 == HIGH) {
-    Serial.println(count++);  // print and increment the number
-  delay(500); 
+    count1++;
+    Serial.print(count2);
+    Serial.println(count1);
+
+    //Calls the numberdisp() function
+    numberdisp();
+    //1 Minute Delay
+  delay(60000);
   } else if
+
+//If button3 is pressed, add -1 to count Var
     (buttonState3 == HIGH) {
-    Serial.println(count--);  // print and increment the number
-  delay(500); 
+    count1= 0;
+    count2= 0;
+    Serial.print(count2);
+    Serial.println(count1);
+    
+    //Calls the numberdisp() function
+    numberdisp();
+    
+  delay(100);
   } else{
-  Serial.println(count);  // print and increment the number
+    Serial.print(count2);
+    Serial.println(count1);
+
+    //Calls the numberdisp() function
+    numberdisp();
+    
   delay(100);
   }
 
-  if (count == 10){
-    count = 0;
-  } else if(count < 0){
-    count = 9;
+// count1 range between 0 - 9
+// If count increments from 9, count = 0
+// If count decrements from 0, count = 9
+  if (count1 == 10){
+    count1 = 0;
+    count2++;
+  } else if(count1 < 0){
+    if(count2 > 0){
+    count1 = 9;
+    count2--;
+    } else {
+      count1 = 0;
+    }
   }
 
-  if (count == 0){
+  if (count2 < 0){
+    count2 = 0;
+  } else if (count2 == 10){
+   count2 = 0; 
+  }
+}
+
+void numberdisp(){
+//Checks the count1 Var and displays it in Screen1
+  if (count1 == 0){
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -55,7 +149,7 @@ void loop() {
     digitalWrite(E, HIGH);
     digitalWrite(F, HIGH);
     digitalWrite(O, LOW);
-  } else if (count == 1){
+  } else if (count1 == 1){
     digitalWrite(A, LOW);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -63,7 +157,7 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, LOW);
     digitalWrite(O, LOW);
-  } else if(count == 2) {
+  } else if(count1 == 2) {
     
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
@@ -72,7 +166,7 @@ void loop() {
     digitalWrite(E, HIGH);
     digitalWrite(F, LOW);
     digitalWrite(O, HIGH);
-  } else if (count == 3) {
+  } else if (count1 == 3) {
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -80,7 +174,7 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, LOW);
     digitalWrite(O, HIGH);
-  } else if (count == 4) {
+  } else if (count1 == 4) {
     digitalWrite(A, LOW);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -88,7 +182,7 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, HIGH);
     digitalWrite(O, HIGH);
-  } else if (count == 5) {
+  } else if (count1 == 5) {
     digitalWrite(A, HIGH);
     digitalWrite(B, LOW);
     digitalWrite(C, HIGH);
@@ -96,7 +190,7 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, HIGH);
     digitalWrite(O, HIGH);
-  } else if (count == 6) {
+  } else if (count1 == 6) {
     digitalWrite(A, HIGH);
     digitalWrite(B, LOW);
     digitalWrite(C, HIGH);
@@ -104,7 +198,7 @@ void loop() {
     digitalWrite(E, HIGH);
     digitalWrite(F, HIGH);
     digitalWrite(O, HIGH);
-  } else if (count == 7) {
+  } else if (count1 == 7) {
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -112,7 +206,7 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, LOW);
     digitalWrite(O, LOW);
-  } else if (count == 8) {
+  } else if (count1 == 8) {
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -120,7 +214,7 @@ void loop() {
     digitalWrite(E, HIGH);
     digitalWrite(F, HIGH);
     digitalWrite(O, HIGH);
-  } else if (count == 9) {
+  } else if (count1 == 9) {
     digitalWrite(A, HIGH);
     digitalWrite(B, HIGH);
     digitalWrite(C, HIGH);
@@ -128,9 +222,93 @@ void loop() {
     digitalWrite(E, LOW);
     digitalWrite(F, HIGH);
     digitalWrite(O, HIGH);
-  } 
- }
+  }
+
+
+  //Checks the count2 Var and displays it in Screen2
+  if (count2 == 0){
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, HIGH);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, LOW);
+  } else if (count2 == 1){
+    digitalWrite(T, LOW);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, LOW);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, LOW);
+    digitalWrite(Z, LOW);
+  } else if(count2 == 2) {
+    
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, LOW);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, HIGH);
+    digitalWrite(Y, LOW);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 3) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, LOW);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 4) {
+    digitalWrite(T, LOW);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, LOW);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 5) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, LOW);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 6) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, LOW);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, HIGH);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 7) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, LOW);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, LOW);
+    digitalWrite(Z, LOW);
+  } else if (count2 == 8) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, HIGH);
+    digitalWrite(X, HIGH);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, HIGH);
+  } else if (count2 == 9) {
+    digitalWrite(T, HIGH);
+    digitalWrite(U, HIGH);
+    digitalWrite(V, HIGH);
+    digitalWrite(W, LOW);
+    digitalWrite(X, LOW);
+    digitalWrite(Y, HIGH);
+    digitalWrite(Z, HIGH);
+  }
+}
 
   
   
-
